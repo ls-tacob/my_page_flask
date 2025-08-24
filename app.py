1dee2ea1cd8d9330e63771856835c97a1cba682f
Santiago Taco <santiagotacobatson@gmail.com>
2025-08-24 12:41:55 -0500

    feat(semana10: herencia de plantilla)

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,13 +6,22 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/redirigir-usuario', methods=['POST'])
+def usuario_redirect():
+    nombre = request.form['nombre']
+    return redirect(url_for('usuario', nombre=nombre))
 
 @app.route('/usuario/<string:nombre>')
 def usuario(nombre):
-    nombre_limpio = nombre.strip().capitalize()
-    return render_template('admin/usuario.html', nombre=nombre_limpio)
+    return render_template('admin/usuario.html', nombre=nombre)
 
+@app.route('/about/<string:nombre>')
+def about(nombre):
+    return render_template('admin/about.html', nombre=nombre)
 
+@app.route('/contacto/<string:nombre>')
+def contacto(nombre):
+    return render_template('admin/contacto.html', nombre=nombre)
 
 if __name__ == '__main__':
     app.run(debug=True)
